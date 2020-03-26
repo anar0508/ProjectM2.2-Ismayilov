f99c38883d46e485d4749ec8d75e266e3bc8b87c
Anar Ismayilov <anar_ismayilov@ymail.com>
2020-03-26 20:37:20 +0400

    v0.8

---
+++ script.js
@@ -0,0 +1,223 @@
+let res = [{ "bankName": "Газпромбанк", "investName": "Ваш успех", "currency": "RUB", "incomeType": 6.22, "sumMin": 50000, "sumMax": Infinity, "termMin": 12, "termMax": 12, "canDeposit": false },
+{ "bankName": "Кредит Европа Банк", "investName": "Оптимальный на 2 года", "currency": "RUB", "incomeType": 6.45, "sumMin": 100000, "sumMax": Infinity, "termMin": 24, "termMax": 24, "canDeposit": false },
+{ "bankName": "Банк Зенит", "investName": "Праздничный 500+", "currency": "RUB", "incomeType": 6, "sumMin": 30000, "sumMax": Infinity, "termMin": 17, "termMax": 17, "canDeposit": false },
+{ "bankName": "Еврофинанс Моснарбанк", "investName": "Классический", "currency": "RUB", "incomeType": 6.95, "sumMin": 30000, "sumMax": Infinity, "termMin": 12, "termMax": 24, "canDeposit": false },
+{ "bankName": "Джей энд Ти Банк", "investName": "Магнус-Онлайн", "currency": "RUB", "incomeType": 6.8, "sumMin": 100000, "sumMax": Infinity, "termMin": 6, "termMax": 6, "canDeposit": false },
+{ "bankName": "МТС Банк", "investName": "В вашу пользу", "currency": "RUB", "incomeType": 6.75, "sumMin": 50000, "sumMax": Infinity, "termMin": 12, "termMax": 12, "canDeposit": true },
+{ "bankName": "Эс-Би-Ай Банк", "investName": "Свои правила Онлайн", "currency": "RUB", "incomeType": 6.7, "sumMin": 30000, "sumMax": 30000000, "termMin": 24, "termMax": 24, "canDeposit": false },
+{ "bankName": "Банк Уралсиб", "investName": "Прогноз отличный", "currency": "RUB", "incomeType": 6.7, "sumMin": 100000, "sumMax": Infinity, "termMin": 37, "termMax": 37, "canDeposit": false },
+{ "bankName": "Инвестторгбанк", "investName": "ИТБ-Постоянный доход", "currency": "RUB", "incomeType": 6.6, "sumMin": 50000, "sumMax": Infinity, "termMin": 37, "termMax": 37, "canDeposit": false },
+{ "bankName": "Транскапиталбанк", "investName": "ТКБ.Постоянный доход", "currency": "RUB", "incomeType": 6.6, "sumMin": 50000, "sumMax": Infinity, "termMin": 37, "termMax": 37, "canDeposit": false },
+{ "bankName": "Совкомбанк", "investName": "Зимний праздник с Халвой", "currency": "RUB", "incomeType": 5.6, "sumMin": 50000, "sumMax": Infinity, "termMin": 2, "termMax": 2, "canDeposit": true },
+{ "bankName": "Агророс", "investName": "Медовый месяц", "currency": "RUB", "incomeType": 5.51, "sumMin": 20000, "sumMax": Infinity, "termMin": 1, "termMax": 1, "canDeposit": true },
+{ "bankName": "Росдорбанк", "investName": "Онлайн-1", "currency": "RUB", "incomeType": 5.1, "sumMin": 100000, "sumMax": 150000000, "termMin": 1, "termMax": 1, "canDeposit": true },
+{ "bankName": "Национальный Стандарт", "investName": "Сберегательный стандарт", "currency": "RUB", "incomeType": 5.1, "sumMin": 100000, "sumMax": Infinity, "termMin": 1, "termMax": 3, "canDeposit": true },
+{ "bankName": "Россия", "investName": "Морозные узоры", "currency": "RUB", "incomeType": 5, "sumMin": 100000, "sumMax": Infinity, "termMin": 1, "termMax": 1, "canDeposit": true },
+{ "bankName": "Кузнецкий Мост", "investName": "Накопительный", "currency": "RUB", "incomeType": 4.85, "sumMin": 50000, "sumMax": Infinity, "termMin": 1, "termMax": 3, "canDeposit": true },
+{ "bankName": "Тексбанк", "investName": "Универсальный", "currency": "RUB", "incomeType": 4.6, "sumMin": 10000, "sumMax": Infinity, "termMin": 1, "termMax": 1, "canDeposit": true },
+{ "bankName": "Морской Банк", "investName": "Правильным курсом +", "currency": "RUB", "incomeType": 4.55, "sumMin": 100000, "sumMax": Infinity, "termMin": 1, "termMax": 3, "canDeposit": true },
+{ "bankName": "Норвик Банк", "investName": "Лаконичный", "currency": "RUB", "incomeType": 4.5, "sumMin": 500, "sumMax": 50000000, "termMin": 1, "termMax": 1, "canDeposit": true },
+{ "bankName": "Промсельхозбанк", "investName": "Конструктор", "currency": "RUB", "incomeType": 4.5, "sumMin": 10000, "sumMax": Infinity, "termMin": 1, "termMax": 3, "canDeposit": true },
+{ "bankName": "Акибанк", "investName": "Онлайн", "currency": "RUB", "incomeType": 6.5, "sumMin": 1000, "sumMax": Infinity, "termMin": 6, "termMax": 6, "canDeposit": true },
+{ "bankName": "Банк БКФ", "investName": "Ключевой стандарт", "currency": "RUB", "incomeType": 6.5, "sumMin": 100000, "sumMax": Infinity, "termMin": 6, "termMax": 13, "canDeposit": true },
+{ "bankName": "Экспобанк", "investName": "Специальный (в конце срока)", "currency": "RUB", "incomeType": 6.35, "sumMin": 50000, "sumMax": 10000000, "termMin": 6, "termMax": 6, "canDeposit": true },
+{ "bankName": "Инвестторгбанк", "investName": "ИТБ-Пополняемый", "currency": "RUB", "incomeType": 6.15, "sumMin": 50000, "sumMax": 30000000, "termMin": 6, "termMax": 6, "canDeposit": true },
+{ "bankName": "Транскапиталбанк", "investName": "ТКБ.Пополняемый", "currency": "RUB", "incomeType": 6.15, "sumMin": 50000, "sumMax": 30000000, "termMin": 6, "termMax": 6, "canDeposit": true },
+{ "bankName": "Евроазиатский Инвестиционный Банк", "investName": "Классика", "currency": "RUB", "incomeType": 6.1, "sumMin": 100000, "sumMax": Infinity, "termMin": 6, "termMax": 12, "canDeposit": true },
+{ "bankName": "Тимер Банк", "investName": "Надежный выбор", "currency": "RUB", "incomeType": 6, "sumMin": 10000, "sumMax": Infinity, "termMin": 6, "termMax": 6, "canDeposit": true },
+{ "bankName": "Евразийский Банк", "investName": "TURBO MAXIMUM", "currency": "RUB", "incomeType": 6, "sumMin": 30000, "sumMax": 299999, "termMin": 6, "termMax": 6, "canDeposit": true },
+{ "bankName": "Таврический Банк", "investName": "Достижимый (онлайн)", "currency": "RUB", "incomeType": 6, "sumMin": 50000, "sumMax": Infinity, "termMin": 6, "termMax": 6, "canDeposit": true },
+{ "bankName": "Экспобанк", "investName": "Юбилейный 25 (в конце срока)", "currency": "RUB", "incomeType": 6.5, "sumMin": 100000, "sumMax": 20000000, "termMin": 12, "termMax": 12, "canDeposit": true },
+{ "bankName": "Крокус-Банк", "investName": "Ежемесячный доход", "currency": "RUB", "incomeType": 6.35, "sumMin": 50000, "sumMax": Infinity, "termMin": 12, "termMax": 12, "canDeposit": true },
+{ "bankName": "Промсельхозбанк", "investName": "Ваш выбор", "currency": "RUB", "incomeType": 6.3, "sumMin": 10000, "sumMax": Infinity, "termMin": 12, "termMax": 12, "canDeposit": true },
+{ "bankName": "Нацинвестпромбанк", "investName": "Прибыльный", "currency": "RUB", "incomeType": 6.3, "sumMin": 50000, "sumMax": Infinity, "termMin": 12, "termMax": 12, "canDeposit": true },
+{ "bankName": "Ишбанк", "investName": "Накопительный", "currency": "RUB", "incomeType": 6.25, "sumMin": 100000, "sumMax": Infinity, "termMin": 12, "termMax": 12, "canDeposit": true },
+{ "bankName": "Примсоцбанк", "investName": "Новогодний чулок (333 дня)", "currency": "RUB", "incomeType": 6.2, "sumMin": 10000, "sumMax": Infinity, "termMin": 11, "termMax": 11, "canDeposit": true },
+{ "bankName": "Еврофинанс Моснарбанк", "investName": "Пополняемый", "currency": "RUB", "incomeType": 6.75, "sumMin": 1000000, "sumMax": Infinity, "termMin": 12, "termMax": 24, "canDeposit": true },
+{ "bankName": "Евроазиатский Инвестиционный Банк", "investName": "VIP", "currency": "RUB", "incomeType": 6.35, "sumMin": 1000000, "sumMax": Infinity, "termMin": 9, "termMax": 12, "canDeposit": true },
+{ "bankName": "Российская Финансовая Корпорация", "investName": "Универсальный", "currency": "RUB", "incomeType": 6, "sumMin": 5000, "sumMax": Infinity, "termMin": 3, "termMax": 3, "canDeposit": true },
+{ "bankName": "Московский Кредитный Банк", "investName": "МЕГА Онлайн", "currency": "RUB", "incomeType": 5.8, "sumMin": 1000, "sumMax": Infinity, "termMin": 3, "termMax": 5, "canDeposit": true },
+{ "bankName": "Александровский", "investName": "Черника 19/20", "currency": "RUB", "incomeType": 5.6, "sumMin": 20000, "sumMax": Infinity, "termMin": 3, "termMax": 3, "canDeposit": true },
+{ "bankName": "Финанс Бизнес Банк", "investName": "Мандариновый!", "currency": "RUB", "incomeType": 5.6, "sumMin": 50000, "sumMax": Infinity, "termMin": 3, "termMax": 3, "canDeposit": true },
+{ "bankName": "ЦентроКредит", "investName": "Доход Плюс", "currency": "USD", "incomeType": 1.15, "sumMin": 5000, "sumMax": Infinity, "termMin": 3, "termMax": 3, "canDeposit": true },
+{ "bankName": "Совкомбанк", "investName": "Удобный (в долларах)", "currency": "USD", "incomeType": 1, "sumMin": 500, "sumMax": Infinity, "termMin": 3, "termMax": 6, "canDeposit": true },
+{ "bankName": "Веста", "investName": "Веста - Копилка", "currency": "USD", "incomeType": 1, "sumMin": 10000, "sumMax": Infinity, "termMin": 3, "termMax": 6, "canDeposit": true },
+{ "bankName": "Славия", "investName": "Славный Капитал", "currency": "USD", "incomeType": 0.85, "sumMin": 5000, "sumMax": Infinity, "termMin": 3, "termMax": 4, "canDeposit": true },
+{ "bankName": "Роскосмосбанк", "investName": "Комфортный", "currency": "USD", "incomeType": 0.8, "sumMin": 500, "sumMax": Infinity, "termMin": 3, "termMax": 6, "canDeposit": true },
+{ "bankName": "ФорБанк", "investName": "Срочный накопительный", "currency": "USD", "incomeType": 0.8, "sumMin": 10000, "sumMax": 500000, "termMin": 3, "termMax": 3, "canDeposit": true },
+{ "bankName": "Московский Областной Банк", "investName": "Гарантированный доллар", "currency": "USD", "incomeType": 0.75, "sumMin": 50, "sumMax": Infinity, "termMin": 4, "termMax": 4, "canDeposit": true },
+{ "bankName": "Объединенный Резервный Банк", "investName": "ОРБ-Накопительный (в конце срока)", "currency": "USD", "incomeType": 1.6, "sumMin": 1000, "sumMax": Infinity, "termMin": 12, "termMax": 12, "canDeposit": true },
+{ "bankName": "Банк Агора", "investName": "Срочный", "currency": "USD", "incomeType": 1.5, "sumMin": 1000, "sumMax": Infinity, "termMin": 12, "termMax": 12, "canDeposit": true },
+{ "bankName": "Тинькофф Банк", "investName": "СмартВклад (с повышенной ставкой)", "currency": "USD", "incomeType": 1.5, "sumMin": 1000, "sumMax": Infinity, "termMin": 12, "termMax": 12, "canDeposit": true },
+{ "bankName": "Первый Инвестиционный Банк", "investName": "Закон сохранения", "currency": "USD", "incomeType": 1.5, "sumMin": 1000, "sumMax": Infinity, "termMin": 12, "termMax": 12, "canDeposit": true },
+{ "bankName": "Новый Век", "investName": "Сберегательный", "currency": "USD", "incomeType": 1.5, "sumMin": 5000, "sumMax": 20000, "termMin": 12, "termMax": 12, "canDeposit": true }
+];
+
+class Application {
+
+    checkCollectedData() {
+        let data = this.collectData();
+        if (data.initSum <= 0 || isNaN(data.initSum)) {
+            this.showError("Ошибка при вводе начального вклада: сумма должна быть больше нуля и написана цифрами");
+        }
+        else if (data.add < 0 || isNaN(data.add)) {
+            this.showError("Ошибка при вводе пополнений: пополнения не могут быть меньше нуля и размер пополнений должен быть написан цифрами");
+        }
+        else if ((data.term <= 0 || data.term % 1 != 0) || isNaN(data.term)) {
+            this.showError("Ошибка при вводе срока: срок должен быть выше нуля и срок должен быть написан цифрами");
+        }
+        else if (data.currency != "RUB" && data.currency != "USD") {
+            this.showError("Ошибка при вводе валюты, возможные валюты: RUB или USD");
+        }
+        else { return data; }
+    }
+
+    collectData() {
+        return {
+            initSum: +document.getElementById("sum").value,
+            add: +document.getElementById("add").value,
+            term: +document.getElementById("term").value,
+            currency: document.getElementById("currency").value
+        }
+    }
+
+    showError(message) {
+        let falseRes = document.getElementById("false-result");
+        falseRes.innerHTML = message;
+        falseRes.style.display = "block";
+        document.getElementById("result").style.display = "none";
+        console.error("Истина где-то рядом!");
+        return false;
+    }
+
+    checkChoosenBankPoducts(arr) {
+        if (arr.length != 0) {
+            return true;
+        } else this.showWarning("К сожалению, мы не нашли банковских продуктов соответствующих вашим критериям");
+    }
+
+    showWarning(message) {
+        let warningRes = document.getElementById("false-result");
+        warningRes.innerHTML = message;
+        warningRes.style.display = "block";
+        document.getElementById("result").style.display = "none";
+        return false;
+    }
+}
+
+class BankProduct {
+    constructor(bankName, investName, incomeType, totalSum) {
+        this.bankName = bankName;
+        this.investName = investName;
+        this.incomeType = incomeType;
+        this.totalSum = totalSum;
+    }
+}
+
+class DatabaseOfBankProducts {
+    constructor(arr) {
+        this.arr = arr;
+    }
+
+    filterArr(deps, bankProds) {
+        let filteredArr = bankProds.filter(function (bankProd) {
+            return bankProd.currency == deps.currency;
+        }).filter(function (bankProd) {
+            return bankProd.sumMin <= deps.initSum && bankProd.sumMax >= deps.initSum;
+        }).filter(function (bankProd) {
+            if (!(!bankProd.canDeposit && deps.add > 0)) { return true; }
+        }).filter(function (bankProd) {
+            return bankProd.termMin <= deps.term && bankProd.termMax >= deps.term;
+        });
+        return filteredArr;
+    }
+}
+
+class Deposit {
+    constructor(initSum, add, term, currency) {
+        this.initSum = initSum;
+        this.add = add;
+        this.term = term;
+        this.currency = currency;
+    }
+}
+
+class Calculator {
+    calculate(deposit, arr) {
+        let result = [];
+
+        for (let bankProd of arr) {
+            let totalSum = deposit.initSum;
+            for (let i = 0; i < deposit.term; i++) {
+                totalSum = (totalSum) * (1 + bankProd.incomeType / 100 / 12) + deposit.add;
+            }
+            let option = new BankProduct(bankProd.bankName, bankProd.investName, bankProd.incomeType, totalSum - deposit.add);
+            result.push(option);
+        }
+        return result.sort(function (a, b) { return b.totalSum - a.totalSum });
+    }
+}
+
+class Table {
+    constructor() {
+        this.table = document.getElementById("myTable")
+    }
+
+    drawTable(arr) {
+        let i = 1;
+        for (let elem of arr) {
+            let row = this.table.insertRow(-1);
+            let cell1 = row.insertCell(0);
+            let cell2 = row.insertCell(1);
+            let cell3 = row.insertCell(2);
+            let cell4 = row.insertCell(3);
+            let cell5 = row.insertCell(4);
+
+            cell1.innerHTML = i;
+            i++;
+            cell2.innerHTML = elem.bankName;
+            cell3.innerHTML = elem.investName;
+            cell4.innerHTML = elem.incomeType;
+            cell5.innerHTML = elem.totalSum.toFixed(0);
+        }
+        this.showTable();
+    }
+
+    showTable() {
+        document.getElementById("result").style.display = "block";
+        document.getElementById("false-result").style.display = "none";
+        console.clear();
+    }
+
+    clearTable() {
+        let rows = this.table.rows;
+        let i = rows.length;
+        while (--i) {
+            this.table.deleteRow(i);
+        }
+    }
+}
+
+class Run {
+    showResult() {
+        let app = new Application();
+        let table = new Table();
+        let data;
+        if (app.checkCollectedData() != false) {
+            data = app.checkCollectedData();
+        } else return;
+        let deposit = new Deposit(data.initSum, data.add, data.term, data.currency);
+        let allBankProds = new DatabaseOfBankProducts(res);
+        let filteredBankProds = allBankProds.filterArr(deposit, allBankProds.arr);
+        let calc = new Calculator;
+        let bankProductsToDisplay;
+        if (app.checkChoosenBankPoducts(filteredBankProds)) {
+            bankProductsToDisplay = calc.calculate(deposit, filteredBankProds);
+        } else return; 
+        table.clearTable();
+        table.drawTable(bankProductsToDisplay);
+    }
+}
+
+let run = new Run();
+document.getElementById("but").addEventListener("click", run.showResult);
